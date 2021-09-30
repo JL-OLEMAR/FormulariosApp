a75a704d68dc81a7477499e1220db5a82ce108b2
JL-OLEMAR <joseluis19963@gmail.com>
2021-09-30 03:26:21 -0500

    Add the Lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-
+  {
+    path: 'template',
+    loadChildren: async () => await import('./template/template.module').then(m => m.TemplateModule)
+  },
+  {
+    path: 'reactive',
+    loadChildren: async () => await import('./reactive/reactive.module').then(m => m.ReactiveModule)
+  },
+  { path: '**', redirectTo: 'template' }
 ]
 
 @NgModule({
