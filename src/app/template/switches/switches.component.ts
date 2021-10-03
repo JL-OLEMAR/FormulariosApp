b4e38292aa56ddd8a48ec16bd119a58b1d5198b7
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-03 06:41:04 -0500

    Template: Radio, Check y Switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class SwitchesComponent implements OnInit {
-  // constructor () { }
-
-  ngOnInit (): void {
+export class SwitchesComponent {
+  persona = {
+    genero: 'M',
+    notificaciones: true
   }
+
+  terminosYCondiciones: boolean = false
 }
