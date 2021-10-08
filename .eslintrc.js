fa6efd3465b7d3f2815ea4805af5da0c780535eb
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-08 15:58:33 -0500

    Actualizacion de archivos

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,6 @@ const RULES = {
 }
 
 module.exports = {
-  parser: './tsconfig.json',
-  extends: ['./node_modules/ts-standard/eslintrc.json'],
-  ignorePatterns: [
-    'node_modules/**',
-    'dist/**'
-  ],
   rules: {
     '@typescript-eslint/no-useless-constructor': RULES.OFF,
     '@typescript-eslint/no-unused-vars': RULES.WARN
